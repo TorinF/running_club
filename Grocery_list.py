edeb996d77f01f3b04d366006c59bf3a767175f8
TorinF <32659879+TorinF@users.noreply.github.com>
2017-10-10 17:01:36 -0700

    Updating the masta
    Using repl.it code to get rid of the old variables

--- Grocery_list.py
+++ Grocery_list.py
@@ -1,36 +1,12 @@
 walker = 0
-<<<<<<< HEAD
 best_so_far_name = None
-best_so_far_cost = None
-heaviest_so_far_weight = None
+best_so_far_cost = 0
+heaviest_so_far_weight = 0
 heaviest_so_far_name = None
 new_entry_name = None
 program_on = True
-
-=======
-best_so_far_name = 0
-best_so_far_cost = 0
-heaviest_so_far_weight = 0
-heaviest_so_far_name = 0
-<<<<<<< HEAD
-=======
-#Danke meme test incoming
-#Test 2
->>>>>>> Changes
-decide = input("What do you want to do with the list.\nsearch/highest price/heaviest/display items/: ")
->>>>>>> aebc96a3a22a222cdb01e1022b4d172f583cc77f
 #Grocery list stats [cost, weight(lbs)]
-apple = [2.5, 1]
-bannana = [1, 1]
-salt = [5, .5]
-pepper = [4, .5]
-watermelon = [10, 20]
-wine = [30, 3]
-snickers = [2, 0.5]
-cavliar = [60, 0.5]
-sunscreen = [5, 1]
-gold = [1285.35, 1]
-#new system
+#new organization variables for item stats
 item_1 = [2.5, 1]
 item_2 = [1, 1]
 item_3 = [5, .5]
@@ -45,16 +21,18 @@ item_11 = [0,0]
 #grocery list names and stat lists
 grocery_list = ['apple', 'bannana', 'salt', 'pepper', 'watermelon',
     'wine', 'snickers', 'cavliar', 'sunscreen','gold']
-grocery_stats = [apple, bannana, salt, pepper, watermelon,
-    wine, snickers, cavliar, sunscreen, gold]
 grocery_items = [item_1, item_2, item_3, item_4, item_5,
-	item_6,item_7,item_8,item_9,item_10,item_11]
+	item_6, item_7, item_8, item_9, item_10, item_11]
 grocery_dict = {'apple': item_1, 'bannana': item_2, 'salt': item_3, 'pepper': item_4, 'watermelon': item_5,
-    'wine': item_6,'snickers': item_7, 'cavliar': item_8, 'sunscreen': item_9, 'gold': item_10,
-	new_entry_name: item_11
+    'wine': item_6,'snickers': item_7, 'cavliar': item_8, 'sunscreen': item_9, 'gold': item_10, new_entry_name: item_11
 }
+print(new_entry_name != None and (not new_entry_name in grocery_list))
 #starting program based on user's decision
 while program_on:
+	#refreshing dictionary and stats incase the user added a new item
+	if new_entry_name != None and (not new_entry_name in grocery_list):
+		grocery_list.insert(len(grocery_list),new_entry_name)
+		grocery_dict = {'apple': item_1, 'bannana': item_2, 'salt': item_3, 'pepper': item_4, 'watermelon': item_5, 'wine': item_6,'snickers': item_7, 'cavliar': item_8, 'sunscreen': item_9, 'gold': item_10, new_entry_name: item_11}
 	grocery_items = [item_1, item_2, item_3, item_4, item_5,
 		item_6, item_7, item_8, item_9, item_10, item_11]
 	decide = input("What do you want to do with the list.\nChoices: search/highest price/heaviest\ndisplay items/search stats/new entry: ")
@@ -78,21 +56,19 @@ while program_on:
 	elif decide == 'highest price':
 		walker = 0
 		for i in range(len(grocery_list)):
-			if grocery_stats[walker][0] > best_so_far_cost:
-				best_so_far_cost = grocery_stats[walker][0]
+			if grocery_items[walker][0] > best_so_far_cost:
+				best_so_far_cost = grocery_items[walker][0]
 				best_so_far_name = grocery_list[walker]
 				walker += 1
 			if walker == len(grocery_list):
 				print(best_so_far_name+" is the most expensive item.")
 				break
-
 	elif decide == 'heaviest':
 	    for i in range(len(grocery_list)):
-	    	#if grocery_dict[walker][1] > heaviest_so_far_weight:
-	        if grocery_stats[walker][1] > heaviest_so_far_weight:
-	            heaviest_so_far_weight = grocery_stats[walker][1]
+	        if grocery_items[walker][1] > heaviest_so_far_weight:
+	            heaviest_so_far_weight = grocery_items[walker][1]
 	            heaviest_so_far_name = grocery_list[walker]
-	        walker = walker + 1
+	        walker += 1
 	        if walker == len(grocery_list):
 	            print(heaviest_so_far_name+" is the heaviest item.")
 	            break
@@ -100,15 +76,16 @@ while program_on:
 	elif decide == 'display items':
 	    print('-------------------')
 	    for i in range(len(grocery_list)):
-	        if grocery_stats[walker][0] > 0 and grocery_stats[walker][1] > 0:
-	        	print(grocery_list[walker]+'\nCost: $'+str(grocery_stats[walker][0])
-	            	+'\nWeight: '+str(grocery_stats[walker][1])+' lbs'+'\n-------------------')
+	        if grocery_items[walker][0] > 0 and grocery_items[walker][1] > 0:
+	        	print(grocery_list[walker]+'\nCost: $'+str(grocery_items[walker][0]))
+	            print('Weight: '+str(grocery_items[walker][1])+' lbs')
+	            print("-------------------")
 	        walker += 1
 	    walker = 0
 	elif decide == 'new entry':
 		new_entry_name = input("What do you want to add?: ")
-		item_11[0] = input("How much does it cost: ")
-		item_11[1] = input("How much does it weight?: ")
+		item_11[0] = int(input("How much does it cost: "))
+		item_11[1] = int(input("How much does it weight?: "))
 		print("Added item to list")
 	else:
 	    print("Invalid input")
